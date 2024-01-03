118504306f34362d0ec3c46d345c41b2a6d52ee6
ShivamRjpoot <154238761+ShivamRjpoot@users.noreply.github.com>
2024-01-03 15:50:22 +0000

    This file is completed. Will start with new file

--- SHIVAM/test.js
+++ SHIVAM/test.js
@@ -1,1 +1,16 @@
 console.log("This is Shivam");
+// let abc=Symbol("123");
+let abc1=Symbol("123");
+// console.log(abc == abc1);
+let bcd =87092740984308832098432053297092984309532845408432532983215932n;
+console.log("SHIVAM",typeof bcd);
+
+
+
+// Practicing String merging through string interpolation
+
+const name ='Shivam';
+let age=22;
+let abc = new String("SHIVAM");
+let str=`My name is ${name} and I'm ${age} year old.`
+// console.log(abc,str);
